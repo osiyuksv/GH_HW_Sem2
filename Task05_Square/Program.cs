4e32f0a74400f8fcf07f89d4731f7d850e993c53
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 23:58:24 +0300

    Task05_Square

---
+++ Task05_Square/Program.cs
@@ -0,0 +1,16 @@
+﻿// выводим является ли одно двухзначное 
+// число квадратом другого
+
+int num1= new Random().Next(1, 100); // указываем правое число +1 от заданного диапазона
+Console.WriteLine(num1);
+int num2= new Random().Next(1, 100); // указываем правое число +1 от заданного диапазона
+Console.WriteLine(num2);
+
+if((num1 == num2 * num2) || (num2 == num1 * num1))
+{
+  Console.WriteLine("да");
+}
+else
+{
+       Console.Write("нет");
+}
