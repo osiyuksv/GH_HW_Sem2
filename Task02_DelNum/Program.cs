b8c87fd6fc76b2ba0da1184694cc4eb3b1ae6fc9
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 22:51:08 +0300

    Task02_DelNum

---
+++ Task02_DelNum/Program.cs
@@ -0,0 +1,12 @@
+﻿// вывести случайно выведенное трёхзначного число без средней цифры
+
+
+int number = new Random().Next(100, 1000); // указываем правое число +1 от заданного диапазона
+Console.WriteLine(number);
+
+int num1 = number / 100;
+int num2 = number % 100 / 10;
+int num3 = number % 10;
+
+Console.Write(num1);
+Console.Write(num3);
