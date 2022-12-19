2df4f53a1eacf37b3acd463ffe573b1a1632b376
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 22:18:31 +0300

    Task01_MaxRandom

---
+++ Task01_MaxRandom/Program.cs
@@ -0,0 +1,17 @@
+﻿// вывести большую цифру из двухзначного числа [10, 99]
+
+int number = new Random().Next(10, 100); // указываем правое число +1 от заданного диапазона
+Console.WriteLine(number);
+
+int num1 = number / 10;
+int num2 = number % 10;
+
+if(num1 > num2)
+{
+  Console.WriteLine(num1);
+}
+else
+{
+   Console.WriteLine(num2);
+}
+
